8b1c04b1d7c31de355440e587d822f4935b8b0e5
ricardofbp <ricardobaiapereira@gmail.com>
2018-11-21 14:52:31 +0000

    stop at low energy

--- tw_script.js
+++ tw_script.js
@@ -3,10 +3,12 @@ var posX = parseInt(window.prompt("Insert job x-pos", 0));
 var posY = parseInt(window.prompt("Insert job y-pos"), 0);
 var duration = parseInt(window.prompt("Insert job duration", 0));
 var intervalBetweenJobs =  parseInt(window.prompt("Insert interval between jobs", 0));
-
+var checkEnergy, doJob;
 var jobCounter = 0;
 
 var badInput = false;
+
+recordFp = function(){}; //disables anticheat???
 
 function validateInputs() 
 {
@@ -19,19 +21,31 @@ function validateInputs()
 
 var automate = function()
 {
-	if (!badInput) 
+	if (!badInput)
 	{
 		automate = function(){}; // kill it as soon as it was called 
-		setInterval(function()
+
+		doJob = setInterval(function()
 		{
 			JobWindow.startJob(jobId, posX, posY, duration);
+			jobCounter = jobCounter + 1;
 			console.log("Job done " + jobCounter + " times");
+			
 		}, intervalBetweenJobs);
-    }
+    	
+    	checkEnergy = setInterval(function()
+		{
+			console.log("Checking energy: " + Character.energy);
+			if (Character.energy <= 10) {
+				clearInterval(checkEnergy);
+				clearInterval(doJob);
+				//TODO go to town and sleep
+			}
+				
+		}, intervalBetweenJobs * 2);
+	}
 
     else alert("Bad input, restart the script");
-
 }
 
 automate();
-
